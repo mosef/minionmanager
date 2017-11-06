0977d1a5a4d76ec309e96f4a62b1481a9533f867
Mosef Asad <mosefasad@gmail.com>
2017-11-06 17:23:47 -0500

    changed port

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ global.DATABASE_URL ||
 exports.TEST_DATABASE_URL = (
  process.env.TEST_DATABASE_URL ||
  'mongodb://localhost/minion-app');
-exports.PORT = process.env.PORT || 27017;
+exports.PORT = process.env.PORT || 8080;
