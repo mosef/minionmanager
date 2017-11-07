356ba89e1cfac14b5500fe85bd76509aaba1e4fb
Mosef Asad <mosefasad@gmail.com>
2017-11-07 02:50:58 -0500

    added seedcampaigndata and required playerChar from models, added teardown function and before/after each functions

--- test/test-server.js
+++ test/test-server.js
@@ -1,25 +1,92 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const faker = require('faker');
+const playerChar = require('../models')
 const {app, runServer, closeServer} = require('../server');
 const { TEST_DATABASE_URL } = require('../config');
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe('Campaign Management testing', function() {
+function seedCampaignData() {
+    console.info('seeding campaign data');
+    const seedData = [];
+    for (let i=1; i<=1; i++) {
+        seedData.push({
+            campaign: {
+                campaignName: faker.company.companyName(),
+                players: [
+                    {
+                        charName: faker.name.firstName(),
+                        statSheet: faker.internet.url(),
+                        email: faker.internet.email(),
+                        session: 2,
+                        expGained: 300,
+                        currentLoot: faker.commerce.product()
+                      },
+                      {
+                        charName: faker.name.firstName(),
+                        statSheet: faker.internet.url(),
+                        email: faker.internet.email(),
+                        session: 2,
+                        expGained: 300,
+                        currentLoot: faker.commerce.product()
+                      },
+                      {
+                        charName: faker.name.firstName(),
+                        statSheet: faker.internet.url(),
+                        email: faker.internet.email(),
+                        session: 2,
+                        expGained: 300,
+                        currentLoot: faker.commerce.product()
+                      },
+                      {
+                        charName: faker.name.firstName(),
+                        statSheet: faker.internet.url(),
+                        email: faker.internet.email(),
+                        session: 2,
+                        expGained: 300,
+                        currentLoot: faker.commerce.product()
+                      }
+                ]
+            }
+        });
+    }
+    return playerChar.insertMany(seedData);
+}
+function teardownDb() {
+    return new Promise((resolve, reject) => {
+        console.warn('Deleting Database');
+        mongoose.connection.dropDatabase()
+        .then(result => resolve(result))
+        .catch(err => reject(err))
+    });
+}
+
+describe('Campaign Manager API resource', function() {
 
     before(function() {
         return runServer(TEST_DATABASE_URL);
       });
-      after(function() {
-        return closeServer();
-      });
-      
-    it('should list campaign details on GET', function() {
-    return chai.request(app)
-        .get('/load')
-        .then(function(res) {
-        res.should.have.status(200);
-        res.should.be.json;
-        })
+    beforeEach(function() {
+        return seedCampaignData();
+    });
+    afterEach(function() {
+        return teardownDb();
     })
+    after(function() {
+        return closeServer();
+    });
+      
 })
+
+describe('GET endpoint', function() {
+    it('should list campaign details on GET', function() {
+        return chai.request(app)
+            .get('/load')
+            .then(function(res) {
+            res.should.have.status(200);
+            res.should.be.json;
+            })
+        })
+       
+});
